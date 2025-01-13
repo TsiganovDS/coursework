2493c938ce328eaf0a42d91ee7868f640a4c1e86
TsiganovDS <118276245+TsiganovDS@users.noreply.github.com>
2025-01-13 23:08:48 +0700

    Update reports.py

--- src/reports.py
+++ src/reports.py
@@ -1,28 +1,23 @@
 import json
-import os
-from datetime import datetime
-from functools import wraps
-from typing import Any, Callable, Optional
-
 import pandas as pd
+from datetime import datetime
+from typing import Callable, Any, Optional
+from functools import wraps
 from dateutil.relativedelta import relativedelta
 
-from src.logger import setting_logger
-
-file_json = os.path.join(os.path.dirname(__file__), "..", "logs", "log_file.json")
-
-logger = setting_logger("reports")
+from src.services import logger
+from src.utils import file_json
 
 
 def save_to_file_decorator(file_json: str) -> Callable:
     def decorator(func: Callable) -> Callable:
         @wraps(func)
-        def wrapper(*args: Any, **kwargs: Any) -> Any:
-            result = func(*args, **kwargs)
+        def wrapper(args: Any, *kwargs: Any) -> Any:
+            result = func(args, *kwargs)
             logger.info("Декоратор записывает полученный результат в файл.")
             with open(file_json, "w", encoding="utf-8") as file:
-                json.dump(result.to_dict("records"), file, ensure_ascii=False, indent=4)
-            logger.info("Декоратор успешно завершил свою работу.")
+                json.dump(result, file, ensure_ascii=False, indent=4)
+                logger.info("Декоратор успешно завершил свою работу.")
             return result
 
         return wrapper
@@ -30,23 +25,31 @@ def save_to_file_decorator(file_json: str) -> Callable:
     return decorator
 
 
-@save_to_file_decorator(file_json)
-def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
-    """Функция, возвращающая транзакции за 3 месяца по определенной категории."""
+@save_to_file_decorator(file_json)  # Укажи путь к файлу
+def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None):
+    """Функция, возвращающая транзакции за 3 месяца по определенной категории в формате JSON."""
     if date is None:
-        end_date = datetime.now().date()
+        end_date = datetime.now()
     else:
         end_date = datetime.strptime(date, "%d.%m.%Y %H:%M:%S")
+
     start_date = end_date - relativedelta(months=3)
 
-    transactions["Дата операции"] = pd.to_datetime(transactions["Дата операции"], format="%d.%m.%Y %H:%M:%S")
-    sorted_transactions_by_date = transactions[
-        (transactions["Дата операции"] >= start_date) & (transactions["Дата операции"] <= end_date)
-    ]
-    pd.options.mode.chained_assignment = None
-    sorted_transactions_by_date["Дата операции"] = sorted_transactions_by_date["Дата операции"].dt.strftime(
-        "%d.%m.%Y %H:%M:%S"
-    )
-    sorted_transactions_by_category = sorted_transactions_by_date[sorted_transactions_by_date["Категория"] == category]
+    # Преобразуем столбец "Дата операции" в формат datetime
+    transactions["Дата операции"] = pd.to_datetime(transactions["Дата операции"], format="%d.%m.%Y %H:%M:%S", errors='coerce')
 
-    return sorted_transactions_by_category
+    # Удаляем записи с некорректными датами
+    transactions = transactions.dropna(subset=["Дата операции"])
+
+    sorted_transactions_by_date = transactions[
+        (transactions["Дата операции"] >= start_date) & (transactions["Дата операции"] <= end_date) &
+        (transactions["Категория"] == category)  # Фильтруем по категории
+    ]
+
+    sort_dict = sorted_transactions_by_date[["Дата операции", "Сумма операции с округлением", "Категория"]].to_dict(orient='records')
+
+    # Преобразуем формат даты в строку
+    for record in sort_dict:
+        record['Дата операции'] = record['Дата операции'].strftime("%d.%m.%Y %H:%M:%S")
+
+    return sort_dict
