40995f161274f28faaebd51892c0afbd360a5a2f
TsiganovDS <118276245+TsiganovDS@users.noreply.github.com>
2025-01-13 09:19:54 +0700

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,19 +1,28 @@
 import logging
 import os
 
-
-def setting_logger(name: str) -> logging.Logger:
+def setting_logger(name: str, level=logging.INFO) -> logging.Logger:
     logger = logging.getLogger(name)
     path = os.path.dirname(os.path.abspath(__file__))  # Получаем путь к текущему файлу
-    log_file_path = os.path.join(path, "../logs", f"{name}.log")  # Формируем путь к лог-файлу
+    logfilepath = os.path.join(path, "../logs", f"{name}.log")  # Формируем путь к лог-файлу
 
-    os.makedirs(os.path.dirname(log_file_path), exist_ok=True)  # Создаём папку, если её нет
+    os.makedirs(os.path.dirname(logfilepath), exist_ok=True)  # Создаём папку, если её нет
 
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s %(module)s %(funcName)s %(levelname)s: %(message)s",
-        filename=log_file_path,
-        filemode="w",
-        encoding="utf-8",
-    )
+    logger.setLevel(level)  # Устанавливаем уровень логирования
+
+    # Формат для логов
+    formatter = logging.Formatter("%(asctime)s %(module)s %(funcName)s %(levelname)s: %(message)s")
+
+    # Обработчик для записи в файл
+    filehandler = logging.FileHandler(logfile_path, mode='w', encoding='utf-8')
+    file_handler.setFormatter(formatter)
+
+    # Обработчик для вывода в консоль
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+
     return logger
