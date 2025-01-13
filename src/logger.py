7b003951d824001d90079b3b05427418e3e0d921
Dmitriy <dm.tsiganov@gmail.com>
2025-01-13 20:30:20 +0700

    3

--- src/logger.py
+++ src/logger.py
@@ -2,18 +2,20 @@ import logging
 import os
 
 
-def setting_logger(name: str) -> logging.Logger:
+def setting_logger(name: str, level=logging.INFO) -> logging.Logger:
     logger = logging.getLogger(name)
-    path = os.path.dirname(os.path.abspath(__file__))  # Получаем путь к текущему файлу
-    log_file_path = os.path.join(path, "../logs", f"{name}.log")  # Формируем путь к лог-файлу
 
-    os.makedirs(os.path.dirname(log_file_path), exist_ok=True)  # Создаём папку, если её нет
+    path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs")
+    os.makedirs(path, exist_ok=True)
+    log_file_path = os.path.join(path, f"{name}.log")
 
     logging.basicConfig(
-        level=logging.INFO,
+        level=level,
         format="%(asctime)s %(module)s %(funcName)s %(levelname)s: %(message)s",
         filename=log_file_path,
         filemode="w",
         encoding="utf-8",
     )
+
     return logger
+
--- src/main.py
+++ src/main.py
@@ -14,12 +14,9 @@ def main() -> None:
         print(generator_json_data(df_transactions, "31.12.2021 23:59:59"))
 
     def action_2() -> Any:
-        print(
-            f"\n{sp}Выберите сервис:\n{sp}1. Поиск переводов физическим лицам \n{sp}"
-            f"2. По переводам на мобильные номера"
-        )
         while True:
-            user_input = input()
+            user_input = input(f"{sp}1.Поиск переводов физическим лицам \n{sp}2. По переводам на мобильные номера\n{sp}Номер : ")
+
             if user_input == "1":
                 print(transfers_to_individuals(df_transactions))
                 break
@@ -28,19 +25,56 @@ def main() -> None:
                 break
 
     def action_3() -> None:
-        while True:
-            category = input("Введите категорию: ")
+        user_input = input(f"\n{sp}1.Супермаркеты \n{sp}2. Каршеринг \n{sp}3. Развлечения\n{sp}4.Фастфуд \n{sp}5. Связь \n{sp}6.Местный транспорт \n{sp}7.Косметика\n{sp}Выберите категорию: ")
+
+        if user_input == "1":
+            category = "Супермаркеты"
             if category in df_transactions["Категория"].values:
                 data_dict = spending_by_category(df_transactions, category, "31.12.2021 23:59:59").to_dict("records")
                 print(data_dict)
-                break
+
+        elif user_input == "2":
+            category = "Каршеринг"
+            if category in df_transactions["Категория"].values:
+                data_dict = spending_by_category(df_transactions, category, "31.12.2021 23:59:59").to_dict("records")
+                print(data_dict)
+
+        elif user_input == "3":
+            category = "Развлечения"
+            if category in df_transactions["Категория"].values:
+                data_dict = spending_by_category(df_transactions, category, "31.12.2021 23:59:59").to_dict("records")
+                print(data_dict)
+
+        elif user_input == "4":
+            category = "Фастфуд"
+            if category in df_transactions["Категория"].values:
+                data_dict = spending_by_category(df_transactions, category, "31.12.2021 23:59:59").to_dict("records")
+                print(data_dict)
+
+        elif user_input == "5":
+            category = "Связь"
+            if category in df_transactions["Категория"].values:
+                data_dict = spending_by_category(df_transactions, category, "31.12.2021 23:59:59").to_dict("records")
+                print(data_dict)
+
+        elif user_input == "6":
+            category = "Местный транспорт"
+            if category in df_transactions["Категория"].values:
+                data_dict = spending_by_category(df_transactions, category, "31.12.2021 23:59:59").to_dict("records")
+                print(data_dict)
+
+        elif user_input == "7":
+            category = "Косметика"
+            if category in df_transactions["Категория"].values:
+                data_dict = spending_by_category(df_transactions, category, "31.12.2021 23:59:59").to_dict("records")
+                print(data_dict)
+
+
 
     print("*" * 30 + " SKY BANK " + "*" * 30)
     print(" " * 9 + "Внимание!!! Для отчета используется дата: 31.12.2021")
-    print(f"{sp}Выберите категорию:\n{sp}1. Главная страница\n{sp}2. Сервисы\n{sp}3. Отчеты")
-
+    user_input = input(f"{sp}1.Главная страница\n{sp}2.Сервисы\n{sp}3.Отчеты\n{sp}Номер: ")
     while True:
-        user_input = input()
         if user_input == "1":
             action_1()
             break
