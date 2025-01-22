06bd3c215f961a47e60fc538249aadd5658727d0
TsiganovDS <118276245+TsiganovDS@users.noreply.github.com>
2025-01-22 15:46:53 +0700

    reports.py

--- src/reports.py
+++ src/reports.py
@@ -1,4 +1,5 @@
 import json
+import os
 from datetime import datetime
 from functools import wraps
 from typing import Any, Callable, Optional
@@ -10,15 +11,24 @@ from src.logger import setting_logger
 
 logger = setting_logger("reports")
 
+file_rep = os.path.join(os.path.dirname(__file__), "..", "logs", "reports.json")
 
-def save_to_file_decorator(file_log: str) -> Callable:
+
+def save_to_file_decorator(file_rep: Optional[str] = "report.json") -> Callable:
     def decorator(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
             result = func(*args, **kwargs)
             logger.info("Декоратор записывает полученный результат в файл.")
-            with open(file_log, "w", encoding="utf-8") as file:
-                json.dump(result.to_dict("records"), file, ensure_ascii=False, indent=4)
+            # Проверяем, является ли результат списком или DataFrame
+            if isinstance(result, list):
+                with open(file_rep, "w", encoding="utf-8") as file:
+                    json.dump(result, file, ensure_ascii=False, indent=4)
+            elif isinstance(result, pd.DataFrame):
+                with open(file_rep, "w", encoding="utf-8") as file:
+                    json.dump(result.to_dict("records"), file, ensure_ascii=False, indent=4)
+            else:
+                logger.error("Неверный тип результата: не список и не DataFrame.")
             logger.info("Декоратор успешно завершил свою работу.")
             return result
 
@@ -27,6 +37,7 @@ def save_to_file_decorator(file_log: str) -> Callable:
     return decorator
 
 
+@save_to_file_decorator(file_rep)
 def spending_by_category(df_transactions: pd.DataFrame, category: str, date: Optional[str] = None):
     """Функция, возвращающая транзакции за 3 месяца по определенной категории в формате JSON."""
     if date is None:
@@ -36,28 +47,27 @@ def spending_by_category(df_transactions: pd.DataFrame, category: str, date: Opt
 
     start_date = end_date - relativedelta(months=3)
 
-    # Преобразуем столбец "Дата операции" в формат datetime
     df_transactions["Дата операции"] = pd.to_datetime(
         df_transactions["Дата операции"], format="%d.%m.%Y %H:%M:%S", errors="coerce"
     )
 
-    # Удаляем записи с некорректными датами
     df_transactions = df_transactions.dropna(subset=["Дата операции"])
 
     sorted_transactions_by_date = df_transactions[
         (df_transactions["Дата операции"] >= start_date)
         & (df_transactions["Дата операции"] <= end_date)
-        & (df_transactions["Категория"] == category)  # Фильтруем по категории
-    ].sort_values(
-        by="Дата операции"
-    )  # Сортировка по дате
+        & (df_transactions["Категория"] == category)
+    ].sort_values(by="Дата операции")
 
-    sort_dict = sorted_transactions_by_date[["Дата операции", "Сумма операции с округлением", "Категория"]].to_dict(
-        orient="records"
-    )
+    if sorted_transactions_by_date.empty:
+        logger.warning("Нет транзакций для данной категории за указанный период.")
+        return []
 
-    # Преобразуем формат даты в строку
-    for record in sort_dict:
+    result = sorted_transactions_by_date[["Дата операции", "Сумма операции с округлением", "Категория"]]
+
+    result_list = result.to_dict(orient="records")
+
+    for record in result_list:
         record["Дата операции"] = record["Дата операции"].strftime("%d.%m.%Y %H:%M:%S")
 
-    return sort_dict
+    return result_list
